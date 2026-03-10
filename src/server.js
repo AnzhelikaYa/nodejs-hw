da6be63caf941ddfc290b636a22b20b8d5a515b6
Anzhelika Ya <didealika@gmail.com>
2026-03-10 19:03:17 +0200

    commit authentication

--- src/server.js
+++ src/server.js
@@ -6,7 +6,9 @@ import { logger } from "./middleware/logger.js";
 import { notFoundHandler } from "./middleware/notFoundHandler.js";
 import { errorHandler } from "./middleware/errorHandler.js";
 import notesRoutes from "./routes/notesRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
 import { errors } from "celebrate";
+import cookieParser from "cookie-parser";
 
 
 const PORT = process.env.PORT || 3000;
@@ -15,6 +17,7 @@ const app = express();
 // middleware
 app.use(express.json({limit: '100kb'}));
 app.use(cors());
+app.use(cookieParser());
 app.use(logger);
 
 // connection to db
@@ -22,7 +25,7 @@ await connectMongoDB();
 
 
 app.use(notesRoutes);
-
+app.use(authRoutes);
 
 
 // middleware
@@ -30,8 +33,7 @@ app.use(notFoundHandler);
 app.use(errors);
 app.use(errorHandler);
 
-
 app.listen(PORT, () => {
-  console.log(`Server is startet on port ${PORT}`);
+  console.log(`Server is started on port ${PORT}`);
   console.log(`http://localhost:${PORT}`);
 });
