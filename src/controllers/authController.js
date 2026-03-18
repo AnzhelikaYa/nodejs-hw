a92b9badf185398648b73171a561b4d2141ad2a6
Anzhelika Ya <didealika@gmail.com>
2026-03-18 17:32:33 +0200

    add mail and avatar

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,8 +1,13 @@
 import bcrypt from "bcrypt";
 import createHttpError from "http-errors";
+import jwt from 'jsonwebtoken';
+import path from 'path';
+import fs from 'fs/promises';
+import handlebars from 'handlebars';
 import { User } from "../models/user.js";
 import { createSession, setSessionCookies } from "../services/auth.js";
 import { Session } from "../models/session.js";
+import { sendEmail } from "../utils/sendMail.js";
 
 export const registerUser = async (req, res) => {
   const { email, password } = req.body;
@@ -26,6 +31,8 @@ export const registerUser = async (req, res) => {
   res.status(201).json(newUser);
 };
 
+
+
 export const loginUser = async (req, res) => {
   const { email, password } = req.body;
 
@@ -47,6 +54,8 @@ export const loginUser = async (req, res) => {
 
   res.status(200).json(user);
 };
+
+
 
 export const refreshUserSession = async (req, res) => {
   const { sessionId, refreshToken } = req.cookies;
@@ -73,6 +82,8 @@ export const refreshUserSession = async (req, res) => {
   res.status(200).json({ "message": "Session refreshed" });
 };
 
+
+
 export const logoutUser = async (req, res) => {
   const { sessionId } = req.cookies;
 
@@ -85,4 +96,70 @@ export const logoutUser = async (req, res) => {
   res.clearCookie('refreshToken');
 
   res.status(204).send();
+};
+
+
+export const requestResetEmail = async (req, res) => {
+  const { email } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.status(200).json({ message: 'Password reset email sent successfully' });
+  }
+
+  const resetToken = jwt.sign(
+    { sub: user._id, email },
+    process.env.JWT_SECRET,
+    { expiresIn: '15m' }
+  );
+
+  const templatePath = path.resolve('src/templates/reset-password-email.html');
+  const templateSource = await fs.readFile(templatePath, 'utf-8');
+  const template = handlebars.compile(templateSource);
+  const html = template({
+    name: user.username,
+    link: `${process.env.FRONTEND_DOMAIN}/reset-password?token=${resetToken}`,
+  });
+
+  try {
+    await sendEmail({
+      from: process.env.SMTP_FROM,
+      to: email,
+      subject: 'Reset your password',
+      html,
+    });
+  } catch {
+    throw createHttpError(500, 'Failed to send the email, please try again later.');
+  }
+
+  res.status(200).json({
+    message: 'Password reset email sent successfully',
+  });
+};
+
+
+export const resetPassword = async (req, res) => {
+  const { password, token } = req.body;
+
+  let payload;
+  try {
+    payload = jwt.verify(token, process.env.JWT_SECRET);
+  } catch {
+    throw createHttpError(401, 'Invalid or expired token');
+  }
+
+  const user = await User.findOne({ _id: payload.sub, email: payload.email });
+
+  if (!user) {
+    throw createHttpError(404, 'User not found');
+  }
+
+  const hashedNewPassword = await bcrypt.hash(password, 10);
+
+  await User.updateOne({ _id: user._id }, { password: hashedNewPassword });
+
+  await Session.deleteMany({ userId: user._id });
+
+  res.status(200).json({message: 'Password reset successfully'});
 };
